dc1c4355f6b7418fa2aeabfe3089d85f04b297d3
Dhanush Kollipara <dhanush.kollipara@ascendion.com>
2024-07-31 12:57:17 +0530

    initial commit by setting up project

---
+++ Task_31_07/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Hobby from './Hobby';
+import Addnums from './Addnums';
+import Calculator from './Calculator';
+import Student from './Student';
+import { BrowserRouter,Routes,Route, Link } from 'react-router-dom';
+import Products from './Products';
+import Gender from './Gender'
+import GithubAPI from './GithubAPI';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header >
+        </header>
+               
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
