599f84c2a15bc9915810480896fbd502614b157c
Dhanush Kollipara <dhanush.kollipara@ascendion.com>
2024-07-31 12:55:19 +0530

    initial commit by setting up project

--- Task_24_07/src/App.js
+++ Task_24_07/src/App.js
@@ -13,30 +13,8 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <header >
-         <h1> React Assignment </h1>
         </header>
-          {/* <Link to="/Hobby/Hobbies Page/1"> Hobby </Link> 
-          <Link to="/Addnums/Adding Numbers/2"> Addnums </Link> 
-          <Link to="/Calculator/Calculator/3"> Calculator </Link> 
-          <Link to="/Student/Student Details/4"> Student  </Link>
-            
-
-          <br></br>
-          <Routes>
-            <Route path="/Hobby/:title/:id" element={<Hobby/>}/>
-            <Route path="/Addnums/:title/:id" element={<Addnums/>}/>
-            <Route path="/Calculator/:title/:id" element={<Calculator/>}/>
-            <Route path="/Student/:title/:id" element={<Student/>}/>
-          </Routes> 
-          <Link to="/gender"> Show Gender </Link> 
-          <Link to="/products"> Show Products  </Link>
-
-          <Routes>
-            <Route path="/Products" element={<Products/>}/>
-            <Route path="/Gender" element={<Gender/>}/>
-
-          </Routes>  */}
-          <GithubAPI/>         
+               
       </BrowserRouter>
     </div>
   );
--- Task_24_07/src/Footer.js
+++
@@ -1,8 +0,0 @@
-function Footer(){
-    return(
-        <div className="Footer">
-        <h1>This is a Footer</h1>
-        </div>
-    )
-}
-export default Footer;
